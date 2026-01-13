19179a2a484d5c0e38185ed04da24cef7ee11dc8
malsharq <malsharq@ineption.malsharq.42.fr>
2026-01-13 20:06:27 +0300

    done wordpress and connected with mriadb

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -4,6 +4,7 @@ set -e
 mkdir -p /run/mysqld
 chown -R mysql:mysql /run/mysqld
 
+sed -i "s/127.0.0.1/0.0.0.0/" "/etc/mysql/mariadb.conf.d/50-server.cnf"
 cat > /tmp/init.sql <<EOF
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 CREATE DATABASE IF NOT EXISTS ${DB_NAME};
@@ -11,5 +12,6 @@ CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
 GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
+
 
 exec mariadbd --user=mysql --datadir=/var/lib/mysql --init-file=/tmp/init.sql

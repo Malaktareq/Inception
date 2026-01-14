0764cae7a715e6f0daa61ec77ca954e03e070739
malsharq <malsharq@inception2.malsharq.42.fr>
2026-01-14 21:58:51 +0300

    connect http on nginx

---
+++ srcs/requirements/nginx/tools/nginx.sh
@@ -0,0 +1,25 @@
+
+if [ ! -f /etc/nginx/conf.d ]; then
+    rm -f /etc/nginx/sites-enabled/default
+    cat > /etc/nginx/conf.d/default.conf <<'EOF'
+server {
+    listen 80;
+    listen [::]:80;
+    server_name localhost;
+
+    root /var/www/html;
+    index index.php index.html;
+
+    location / {
+        try_files $uri $uri/ /index.php?$args;
+    }
+
+    location ~ \.php$ {
+        include fastcgi_params;
+        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+        fastcgi_pass wordpress:9000;
+    }
+}
+EOF
+
+fi
